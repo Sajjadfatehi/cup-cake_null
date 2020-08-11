6c1c4e95b93049cf745edc82a347b568b2556666
Sajjad.fatehi <sajjadfatehi79@gmail.com>
2020-08-11 14:34:46 +0430

    move from login to sign up

--- app/src/main/java/com/example/anull/LoginFragment.kt
+++ app/src/main/java/com/example/anull/LoginFragment.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class LoginFragment : Fragment() {
+    lateinit var goToBeMember: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        goToBeMember = view.findViewById(R.id.tv_go_to_be_member)
+        goToBeMember.setOnClickListener {
+            val signUpFragment = SignUpFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.splashFrame, signUpFragment).commit()
+
+        }
+
+
+        return view
     }
 
 }
