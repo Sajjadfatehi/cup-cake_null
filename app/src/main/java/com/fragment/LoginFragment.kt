43c008c2d7ddfb16cc230ff31d22a5c8c246e3c0
mohammad <mohammadazizi2976@gmail.com>
2020-08-12 16:21:11 +0430

    fix lgin and register

--- app/src/main/java/com/fragment/LoginFragment.kt
+++ app/src/main/java/com/fragment/LoginFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.anull.R
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
-    lateinit var goToBeMember: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,19 +18,16 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_login, container, false)
-        goToBeMember = view.findViewById(R.id.tv_go_to_be_member)
-        goToBeMember.setOnClickListener {
-            val signUpFragment = SignUpFragment()
-            requireActivity().supportFragmentManager.beginTransaction()
-                .replace(R.id.splashFrame, signUpFragment).commit()
-
-        }
-        setDefault()
         return view
     }
 
-    private fun setDefault() {
-        outlinedTextField2b
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.window?.attributes?.softInputMode
+        tv_go_to_be_member.setOnClickListener {
+            val signUpFragment = SignUpFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.splashFrame, signUpFragment).commit()
+        }
     }
-
 }
--- app/src/main/java/com/fragment/SignUpFragment.kt
+++ app/src/main/java/com/fragment/SignUpFragment.kt
@@ -7,27 +7,26 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.anull.R
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 class SignUpFragment : Fragment() {
-    lateinit var goLoginScreen: TextView
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
-        goLoginScreen = view.findViewById(R.id.tv_go_login_screen)
-        goLoginScreen.setOnClickListener {
-            val loginFragment = LoginFragment()
-            // activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.splashFrame,loginFragment)?.commit()
+        return view
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tv_go_login_screen.setOnClickListener {
+            val loginFragment = LoginFragment()
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.splashFrame, loginFragment).commit()
         }
 
-
-        return view
     }
-
 }
