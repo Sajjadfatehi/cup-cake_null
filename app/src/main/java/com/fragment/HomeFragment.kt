014ea7c641cdc543d44bbb5876cb41082de0ed13
mohammad <mohammadazizi2976@gmail.com>
2020-08-15 18:05:48 +0430

    add tablayout

--- app/src/main/java/com/fragment/HomeFragment.kt
+++ app/src/main/java/com/fragment/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.anull.R
+import com.model.TabModel
+import kotlinx.android.synthetic.main.fragment_home.*
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -13,6 +16,8 @@ class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var tab : Boolean = false
+    private var tabs : MutableList<TabModel> ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -38,5 +43,23 @@ class HomeFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //------------------------------------------------
+        if (!tab) {
+            setTabs()
+            tab = true
+        }
+    }
+
+    private fun setTabs() {
+        tabs?.add(TabModel("tab one"))
+        tabs?.add(TabModel("tab two"))
+        tabs?.add(TabModel("tab there"))
+        tabLayout.addTab(tabLayout.newTab().setText(tabs?.elementAt(0).toString()))
+        tabLayout.addTab(tabLayout.newTab().setText(tabs?.elementAt(1).toString()))
+        tabLayout.addTab(tabLayout.newTab().setText(tabs?.elementAt(2).toString()))
     }
 }
