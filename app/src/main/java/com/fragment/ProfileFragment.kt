9019aaa34d331c57da9df75b4b2b22865dc0e166
mohammad <mohammadazizi2976@gmail.com>
2020-08-24 17:25:30 +0430

    fix

--- app/src/main/java/com/fragment/ProfileFragment.kt
+++ app/src/main/java/com/fragment/ProfileFragment.kt
@@ -7,36 +7,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.adapter.PostsInProfAdapter
 import com.example.anull.R
+import com.example.anull.databinding.FragmentLoginBinding
+import com.example.anull.databinding.FragmentProfileBinding
 import com.model.PostInProf
-import kotlinx.android.synthetic.main.fragment_profile.recycler_posts_in_prof
-import kotlinx.android.synthetic.main.fragment_profile.titleRadioBtn
-import kotlinx.android.synthetic.main.testlayout.*
+import com.model.profile.ProfileModel
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
     private val postLists = mutableListOf<PostInProf>()
-    private val titleInProfList = mutableListOf<String>()
 
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
-//        val view = inflater.inflate(R.layout.        val view = inflater.inflate(R.layout.testlayout, container, false)
-//            , container, false)
-        return view
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        titleInProfList.add("نوشته ها")
-        titleInProfList.add("علاقه مندی")
+
+        binding.user = ProfileModel("محمد عزیزی","برنامه نویس اندروید","214","5010")
         repeat(6) {
             postLists.add(
                 PostInProf(
@@ -49,50 +47,10 @@ class ProfileFragment : Fragment() {
                 )
             )
         }
-//
-        follow_button.setOnClickListener {
-
-            // findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToHomeFragment())
-//            val bottomSheet = BottomSheetFragment()
-//            bottomSheet.show(requireActivity().supportFragmentManager, bottomSheet.tag)
-            findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToTitleFragment())
-
-        }
-        titleRadioBtn.setOnCheckedChangeListener { _, i ->
-
-            // TODO: 22/08/2020 hard code colors
-            val radio = requireActivity().findViewById<RadioButton>(i)
-            // Toast.makeText(requireContext(),"${radio.text}",Toast.LENGTH_SHORT).show()
-            if (radio.tag == "posts") {
-                radio.setTextColor(Color.parseColor("#813ac1"))
-                requireActivity().findViewById<RadioButton>(R.id.radio2)
-                    .setTextColor(Color.parseColor("#363636"))
-            } else {
-                radio.setTextColor(Color.parseColor("#813ac1"))
-                requireActivity().findViewById<RadioButton>(R.id.radio1)
-                    .setTextColor(Color.parseColor("#363636"))
-
-            }
-        }
-
-
-        recycler_posts_in_prof.apply {
+        recycler_my_articles.apply {
             adapter = PostsInProfAdapter(postLists)
+            setHasFixedSize(true)
         }
 
     }
-
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = this.findNavController(R.id.fragment)
-//        return when(navController.currentDestination?.id) {
-//            R.id.redFragment -> {
-//                // custom behavior here
-//                true
-//            }
-//            else -> navController.navigateUp()
-//        }
-//    }
-
-
 }
-
